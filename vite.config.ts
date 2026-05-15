828044a892990b1f3c4905ea343d7123a5a9aa42
CuLoo <140522287+Viruatios@users.noreply.github.com>
2026-05-15 13:09:59 +0800

    Revert "API调用修改为通过代理进行，防止CORS"
    This reverts commit 3a7840051b0b7478898520fd68bbb75d400be5b6.

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  },
-  server: {
-    // 配置代理，将 /api 的请求转发到后端服务器
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
   }
 })
